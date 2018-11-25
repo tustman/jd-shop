d78b9e2df197f2841ec182cded1ec17cd774219b
wangyining <wangyining@ixiaocong.com>
2018-11-25 22:08:00 +0800

    我的页面处理

--- lib/market/market_page.dart
+++ lib/market/market_page.dart
@@ -2,27 +2,22 @@ import 'package:flutter/material.dart';
 import '../global_config.dart';
 
 class MarketPage extends StatefulWidget {
-
   @override
   _MarketPageState createState() => new _MarketPageState();
-
 }
 
 class _MarketPageState extends State<MarketPage> {
-
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
         home: new Scaffold(
           appBar: new AppBar(
-            title: new Text('市场'),
+            title: new Text('转链'),
           ),
           body: new Center(
-              child: null
+            child: new Text("转链页面"),
           ),
         ),
-        theme: GlobalConfig.themeData
-    );
+        theme: GlobalConfig.themeData);
   }
-
 }
--- lib/notice/notice_page.dart
+++ lib/notice/notice_page.dart
@@ -2,27 +2,20 @@ import 'package:flutter/material.dart';
 import '../global_config.dart';
 
 class NoticePage extends StatefulWidget {
-
   @override
   _NoticePageState createState() => new _NoticePageState();
-
 }
 
 class _NoticePageState extends State<NoticePage> {
-
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
         home: new Scaffold(
           appBar: new AppBar(
-            title: new Text('通知'),
+            title: new Text('佣金'),
           ),
-          body: new Center(
-              child: null
-          ),
+          body: new Center(child: new Text('佣金页面')),
         ),
-        theme: GlobalConfig.themeData
-    );
+        theme: GlobalConfig.themeData);
   }
-
 }
